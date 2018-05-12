2f1d0256255986a80f4123dc07ea35e2969ea66e
Mike Simpson <mgsimpson@atomicsauce.org>
2018-05-12 09:50:42 -0700

    Automate pre-build static analysis.
    This commit adds five pre-build static analysis tools:
    
    *   Ancient, which checks for outdated dependencies;
    *   Bikeshed, which checks for code formatting smells;
    *   Cljfmt, which enforces standard Clojure syntax formatting;
    *   Eastwood, a basic Clojure linter;
    *   Kibit, which checks for and advises on idiomatic syntax.
    
    Also in this commit, CircleCI integration is added via
    the .circleci/config.yml file.
    
    This commit corresponds to requirement 197 in the
    TargetProcess tracking system.

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject skein "0.1.0-SNAPSHOT"
+  :description "A wiki written in Clojure."
+  :url "https://github.com/atomicsauce/skein"
+  :license {:name "BSD 2-Clause \"Simplified\" License"
+            :url "https://github.com/atomicsauce/skein/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :main ^:skip-aot skein.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}
+             :testing {:plugins [[lein-ancient "0.6.15"]
+                                 [lein-bikeshed "0.5.1"]
+                                 [lein-cljfmt "0.5.7"]
+                                 [jonase/eastwood "0.2.5"]
+                                 [lein-kibit "0.1.6"]]}})
---
+++ src/skein/core.clj
@@ -0,0 +1,8 @@
+(ns skein.core
+  "The top-level namespace for the Skein application."
+  (:gen-class))
+
+(defn -main
+  "iThe main entry point for the Skein application."
+  [& args]
+  (println "(Skein Core)"))
